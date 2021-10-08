d38af196e0efeb0ae67e3c848eb3ec09b30ecfdc
wesleylucena <61255166+wesleylucena@users.noreply.github.com>
2021-10-07 23:27:19 -0300

    mensagem

---
+++ PlantasDecorativas.Android/obj/Debug/110/android/src/crc64eb434f1a4334fd0b/SplashActivity.java
@@ -0,0 +1,56 @@
+package crc64eb434f1a4334fd0b;
+
+
+public class SplashActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;Landroid/os/PersistableBundle;)V:GetOnCreate_Landroid_os_Bundle_Landroid_os_PersistableBundle_Handler\n" +
+			"n_onResume:()V:GetOnResumeHandler\n" +
+			"";
+		mono.android.Runtime.register ("PlantasDecorativas.Droid.SplashActivity, PlantasDecorativas.Android", SplashActivity.class, __md_methods);
+	}
+
+
+	public SplashActivity ()
+	{
+		super ();
+		if (getClass () == SplashActivity.class)
+			mono.android.TypeManager.Activate ("PlantasDecorativas.Droid.SplashActivity, PlantasDecorativas.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0, android.os.PersistableBundle p1)
+	{
+		n_onCreate (p0, p1);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0, android.os.PersistableBundle p1);
+
+
+	public void onResume ()
+	{
+		n_onResume ();
+	}
+
+	private native void n_onResume ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
